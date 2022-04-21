cf63c0ba06dee3a874253dc9ee4ab2469b358239
kovacsbencet <kovacsbencet@gmail.com>
2022-04-21 17:30:08 +0200

    .

---
+++ index.js
@@ -0,0 +1,21 @@
+const arrayOfStrings = ["bluebird", "bluelake", "blueriver", "bluesky", "bluefire", "redbird", "redlake", "redriver"]
+
+/* const match = arrayOfStrings.find(element => {
+    if (element.includes("blu")) {
+      return true;
+    }
+}); */
+/* 
+const match = arrayOfStrings.filter(match => {
+    if(match.indexOf("blu") !==-1)
+    return true
+});
+
+console.log(match)
+ */
+function search(arr, word){
+    const result = arr.filter(element => element.includes(word))
+    return result
+}
+
+module.exports = search
---
+++ index.test.js
@@ -0,0 +1,5 @@
+const search = require("./index")
+
+it("This is my first test", () => {
+    expect("hello").toEqual("hello")
+})
